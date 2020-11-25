6986d29de2572b2ad3df79a82679d3da5d8e450a
César Cabrejos Rimarachín <cesaragustin1922@gmail.com>
2020-11-24 22:16:28 -0500

    cambios 3

--- app/src/views/Local copy.vue
+++ app/src/views/Local copy.vue
@@ -1,151 +1,51 @@
 <template>
   <v-content>
-    <v-row class="pa-2" align="center">
-      <v-col cols="12">
-        <h2 class="font-weight-regular text-center">
-          Mantenimiento de Local
-        </h2>
+    <v-row align="center" class="pa-5 align-center">
+      <v-col cols="4">
+        <v-alert
+          v-model="alert"
+          text
+          prominent
+          type="error"
+          icon="mdi-cloud-alert"
+          close-text="Close Alert"
+          dismissible
+          >Ocurrio un error.</v-alert
+        >
       </v-col>
     </v-row>
-    <v-row align="center">
-      <v-col cols="11" align="end">
+    <v-row class="pa-5 align-center">
+      <v-col>
         <v-btn
-          color="indigo darken-4 white--text"
-          elevation="5"
+          fab
+          large
+          dark
+          color="blue darken-3"
           @click="dialogEjemplo = true"
         >
-          <v-icon left dark>mdi-plus</v-icon>
-          Local
+          <v-icon>mdi-plus</v-icon>
         </v-btn>
       </v-col>
-      <v-col class="pa-1" align="center">
-        <v-col cols="10" align="center">
-          <v-card style="border-top: 5px solid #1a237e !important">
-            <v-card-title align="center" justify-center class="text-center">
-              <v-row
-                align="center"
-                justify-center
-                class="text-center pb-0 pl-10"
-              >
-                <v-form ref="form" v-model="valid" lazy-validation>
-                  <v-row align="center" justify-center class="text-center">
-                    <v-col
-                      class="justify-content-center text-center"
-                      align="center"
-                      cols="6"
-                    >
-                      <!-- <v-select
-                        :items="items"
-                        v-model="atributo"
-                        label="Busqueda Personalizada"
-                        dense
-                        class="pt-8"
-                        :rules="[fieldRules.required]"
-                        @click="limpiarValidate"
-                      ></v-select> -->
-                    </v-col>
-                    <v-col
-                      class="justify-content-center text-center"
-                      align="center"
-                      cols="5"
-                    >
-                      <!-- <v-text-field
-                        v-model="busqueda"
-                        label="Search"
-                        single-line
-                        hide-details
-                        :rules="[fieldRules.required]"
-                        @click="limpiarValidate"
-                      ></v-text-field> -->
-                    </v-col>
-                    <v-col
-                      class="justify-content-center text-center"
-                      align="center"
-                      cols="1"
-                    >
-                      <!-- <v-btn
-                        icon
-                        color="indigo darken-4"
-                        class="pt-8"
-                        @mousedown="validate"
-                        @click="busquedaUsuario"
-                      >
-                        <v-icon>mdi-magnify</v-icon>
-                      </v-btn> -->
-                    </v-col>
-                  </v-row>
-                </v-form>
-              </v-row>
-            </v-card-title>
-
-            <v-data-table
-      background= "blue darken-3"
-      :headers="headers"
-      :items="locales"
-      :search="search"
-      :item-class="itemFilaColor"
-    >
-      <template v-slot:[`item.index`]="{ item }">
-        {{ locales.indexOf(item) + 1 }}
-      </template>
-      <template v-slot:[`item.actions`]="{ item }">
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <v-icon
-              v-bind="attrs"
-              v-on="on"
-              class="mr-2"
-              color="blue-grey"
-              @click="showactualizar(item)"
-              >mdi-border-color</v-icon
-            >
-          </template>
-          <span>Editar</span>
-        </v-tooltip>
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <v-icon
-              v-bind="attrs"
-              v-on="on"
-              class="mr-2"
-              :color="item.Vigencia ? 'red lighten-1' : 'green' "
-              @click="deleteLocal(item)"
-              >{{
-                item.Vigencia ? "mdi-close-circle-outline" : "mdi-checkbox-marked-circle-outline"
-              }}</v-icon
-            >
-          </template>
-          <span>{{ item.Vigencia ? "Dar de baja" : "Dar de alta" }}</span>
-        </v-tooltip>
-      </template>
-    </v-data-table>
-          </v-card>
-        </v-col>
+      <v-col cols="11">
+        <h2 class="font-weight-regular text-center">Mantenimiento Local</h2>
       </v-col>
     </v-row>
     <v-dialog v-model="dialogEjemplo" persistent scrollable max-width="60vw">
       <v-card>
-        <v-card-title
-          class="headline"
-          style="
-            border-left: 5px solid #1a237e !important;
-            color: #90a4ae !important;
-            background: #e8eaf6 !important;
-          "
-        >
-          <v-icon v-if="edit" style="color: #90a4ae !important"
-            >mdi-account-settings</v-icon
+        <v-card-title class="headline indigo darken-4">
+          <span v-if="edit" class="headline" style="color: white"
+            >Editar Local</span
           >
-          <v-icon v-else style="color: #90a4ae !important"
-            >mdi-account-plus</v-icon
+          <span v-else-if="ver" class="headline" style="color: white"
+            >Ver Local</span
           >
-          <h6 v-if="edit" class="pl-3">Editar Local</h6>
-          <h6 v-else class="pl-3">Nuevo Local</h6>
+          <span v-else class="headline" style="color: white"
+            >Nuevo Local</span
+          >
         </v-card-title>
-        <v-divider></v-divider>
         <v-card-text>
-          <v-form ref="form" v-model="validD" lazy-validation>
-             <v-row>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-row>
               <v-col cols="6">
                 <v-text-field
                   :disabled="ver"
@@ -181,12 +81,14 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn
+            v-if="!ver"
             color="indigo darken-4"
             text
             @click="(dialogEjemplo = false), limpiar()"
             >Cancelar</v-btn
           >
           <v-btn
+            v-if="!ver"
             :loading="saveLoading"
             :disabled="saveLoading"
             color="indigo darken-4"
@@ -196,9 +98,57 @@
             @click="executeEventClick"
             >Guardar</v-btn
           >
+          <v-btn
+            v-if="ver"
+            color="indigo darken-4"
+            text
+            @click="(dialogEjemplo = false), limpiar()"
+            >Cancelar</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-data-table
+      background= "blue darken-3"
+      :headers="headers"
+      :items="locales"
+      :search="search"
+      :item-class="itemFilaColor"
+    >
+      <template v-slot:[`item.index`]="{ item }">
+        {{ locales.indexOf(item) + 1 }}
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              v-bind="attrs"
+              v-on="on"
+              class="mr-2"
+              color="blue-grey"
+              @click="showactualizar(item)"
+              >mdi-pencil</v-icon
+            >
+          </template>
+          <span>Editar</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              v-bind="attrs"
+              v-on="on"
+              class="mr-2"
+              :color="item.Vigencia ? 'red lighten-1' : 'green' "
+              @click="deleteLocal(item)"
+              >{{
+                item.Vigencia ? "mdi-close-circle-outline" : "mdi-checkbox-marked-circle-outline"
+              }}</v-icon
+            >
+          </template>
+          <span>{{ item.Vigencia ? "Dar de baja" : "Dar de alta" }}</span>
+        </v-tooltip>
+      </template>
+    </v-data-table>
   </v-content>
 </template>
 
@@ -220,18 +170,18 @@ export default {
       dialogEjemplo: false,
       itemsEmpresa: [],
       headers:[
-        {text: "Nº", value: "index", width: "10%", class: 'light  darken-4 black--text' },
+        {text: "Nº", value: "index", width: "10%", class: 'light blue darken-4 white--text' },
         {
           text: "Nombre",
           align: "start",
           // sortable: false,
           value: "Nombre",
           width: "25%",
-          class: 'light darken-4 black--text',
+          class: 'light blue darken-4 white--text',
         },
-        { text: "Dirección", value: "Direccion", width: "30%", class: 'light  darken-4 black--text' },
-        { text: "Teléfono", value: "Telefono", width: "20%", class: 'light  darken-4 black--text' },
-        { text: "Acciones", value: "actions", width: "15%", class: 'light  darken-4 black--text' },
+        { text: "Dirección", value: "Direccion", width: "30%", class: 'light blue darken-4 white--text' },
+        { text: "Teléfono", value: "Telefono", width: "20%", class: 'light blue darken-4 white--text' },
+        { text: "Acciones", value: "actions", width: "15%", class: 'light blue darken-4 white--text' },
       ],
       headers2: [
         {
@@ -425,7 +375,9 @@ export default {
     this.mostrarEmpresas();
   },
 };
+
 </script>
 
 <style>
 </style>
+<style></style>
